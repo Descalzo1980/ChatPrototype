c37df71c38e3fc2d9dbbf63d7443ddc9c52bbac1
Stas <ragioniere1980@gmail.com>
2025-06-20 14:37:21 +0300

    Initial commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Станислав Леонов on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
